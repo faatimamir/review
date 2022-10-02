c222b8c377a187341052951b3e564b82db3305e5
faatimamir <faatima.mir@gmail.com>
2022-10-02 07:40:56 +0500

    seting item
    seting item

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ text:"gggggsgggggdggdggdggg",},
     id: 2,
     name: "peter",
     job:"content writter",
-    img:"./pic/p1.jpg",
+    img:"./pic/p2.jpg",
     
     text:"gggggsgggggdggdggdggg",},
     {
         id: 3,
         name: "john",
         job:"content writter",
-        img:"./pic/p1.jpg",
+        img:"./pic/p3.jpg",
         
         text:"gggggsgggggdggdggdggg",},
             ]
@@ -30,11 +30,45 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 const review = document.querySelector(".review")
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+});
+
+
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+nextBtn.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
   
 randomBtn.addEventListener("click", function () {
     console.log("hello");
   
     currentItem = Math.floor(Math.random() * reviews.length);
-   //showPerson(currentItem);
-   review.innerHTML = currentItem
+   showPerson(currentItem);
+    
   });
